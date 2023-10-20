a10b758782648db6af3987a468527de9d8a36737
Nullwell47 <emanuelr.barros16@gmail.com>
2023-10-19 23:44:24 -0300

    criando logica inicial do menu

--- desafio_projeto.py
+++ desafio_projeto.py
@@ -12,3 +12,22 @@ limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        print("Depósito")
+    
+    elif opcao == "2":
+        print("Saque")
+
+    elif opcao == "3":
+        print("Extrado")
+    
+    elif opcao == "0":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
