a8fc6c9945a75542428f435c6613e3cfa512738c
Nullwell47 <emanuelr.barros16@gmail.com>
2023-10-23 21:09:39 -0300

    incluindo operacao de saque, corrigindo texto

--- desafio_projeto.py
+++ desafio_projeto.py
@@ -18,7 +18,7 @@ while True:
     opcao = input(menu)
 
     if opcao == "1":
-        valor = float(input("Informe o valor de depósito: "))
+        valor = float(input("Informe o valor do depósito: "))
         
         if valor > 0:
             saldo += valor
@@ -27,7 +27,30 @@ while True:
             print("Operação falhou! O valor informado é inválido.")
     
     elif opcao == "2":
-        print("Saque")
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+        
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+        
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+        
+        else:
+            print("Operação falhou! O valor informado é inválido.")
 
     elif opcao == "3":
         print("Extrato")
