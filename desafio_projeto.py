b63fed9c330a71938d45f130c607434a7d248072
Nullwell47 <emanuelr.barros16@gmail.com>
2023-11-05 03:11:32 -0300

    refat v1 para v2, incluindo funcs dep, saq e ext

--- desafio_projeto.py
+++ desafio_projeto.py
@@ -14,6 +14,48 @@ def menu():
     ==> """
     return input(textwrap.dedent(menu))
 
+# Função para realizar um depósito em uma conta.
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        saldo += valor  # Incrementa o saldo
+        extrato += f"Depósito: R$ {valor:.2f}\n"  # Adiciona registro no extrato
+        print("\n=== Depósito realizado com sucesso! ===")
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+    return saldo, extrato
+
+# Função para realizar um saque em uma conta.
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    excedeu_saldo = valor > saldo  # Verifica se o valor do saque excede o saldo disponível
+    excedeu_limite = valor > limite  # Verifica se o valor do saque excede o limite
+    excedeu_saques = numero_saques >= limite_saques  # Verifica se excedeu o número máximo de saques
+
+    if excedeu_saldo:
+        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+    
+    elif excedeu_limite:
+        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+    
+    elif excedeu_saques:
+        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+
+    elif valor > 0:
+        saldo -= valor  # Decrementa o saldo
+        extrato += f"Saque: R$ {valor:.2f}\n"  # Adiciona registro no extrato
+        numero_saques += 1  # Incrementa o contador de saques
+        print("\n=== Saque realizado com sucesso! ===")
+    
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+    return saldo, extrato
+
+# Função para exibir o extrato de uma conta.
+def exibir_extrato(saldo, /, *, extrato):
+    print("\n================ EXTRATO ================")
+    print("Não foram realizados movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==========================================")
+
 # Função principal que gerencia as operações bancárias.
 def main():
     # Variáveis
@@ -35,43 +77,24 @@ def main():
         if opcao == "1":
             valor = float(input("Informe o valor do depósito: "))
             
-            if valor > 0:
-                saldo += valor  # Incrementa o saldo
-                extrato += f"Depósito: R$ {valor:.2f}\n"  # Adiciona registro no extrato
-            else:
-                print("Operação falhou! O valor informado é inválido.")
+            saldo, extrato = depositar(saldo, valor, extrato)
 
         # Opção 2: Sacar
         elif opcao == "2":
             valor = float(input("Informe o valor do saque: "))
 
-            excedeu_saldo = valor > saldo  # Verifica se o valor do saque excede o saldo disponível
-            excedeu_limite = valor > limite  # Verifica se o valor do saque excede o limite
-            excedeu_saques = numero_saques >= LIMITE_SAQUES  # Verifica se excedeu o número máximo de saques
-
-            if excedeu_saldo:
-                print("Operação falhou! Você não tem saldo suficiente.")
-            
-            elif excedeu_limite:
-                print("Operação falhou! O valor do saque excede o limite.")
-            
-            elif excedeu_saques:
-                print("Operação falhou! Número máximo de saques excedido.")
-
-            elif valor > 0:
-                saldo -= valor  # Decrementa o saldo
-                extrato += f"Saque: R$ {valor:.2f}\n"  # Adiciona registro no extrato
-                numero_saques += 1  # Incrementa o contador de saques
-            
-            else:
-                print("Operação falhou! O valor informado é inválido.")
+            saldo, extrato = sacar(
+                saldo=saldo,
+                valor=valor,
+                extrato=extrato,
+                limite=limite,
+                numero_saques=numero_saques,
+                limite_saques=LIMITE_SAQUES,
+            )           
 
         # Opção 3: Extrato
         elif opcao == "3":
-            print("\n================ EXTRATO ================")
-            print("Não foram realizados movimentações." if not extrato else extrato)
-            print(f"\nSaldo: R$ {saldo:.2f}")
-            print("==========================================")
+            exibir_extrato(saldo, extrato=extrato)
 
         # Opção 0: Sair
         elif opcao == "0":
