13d96b3bbd7c32e71da185528cd60c8a2230d035
Nullwell47 <emanuelr.barros16@gmail.com>
2023-11-05 04:08:54 -0300

    incluindo funcs cria usuarios, cria e lista conta

--- desafio_projeto.py
+++ desafio_projeto.py
@@ -56,6 +56,54 @@ def exibir_extrato(saldo, /, *, extrato):
     print(f"\nSaldo: R$ {saldo:.2f}")
     print("==========================================")
 
+# Função para criar um novo usuário e adicioná-lo à lista de usuários.
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente números): ")
+    usuario = filtrar_usuario(cpf, usuarios) # Verifica se já existe um usuário com o CPF informado.
+
+    if usuario:
+        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        return
+    
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereco (logradouro, nro - bairro - cidade/sigla estado): ")
+    
+    # Cria um dicionário com as informações do usuário e o adiciona à lista de usuários.
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco}) 
+
+    print("=== Usuário criado com sucesso! ===")
+
+# Função para filtrar um usuário com base no CPF.
+def filtrar_usuario(cpf, usuarios):
+    # Cria uma lista de usuários filtrados, que contém todos os usuários com o CPF especificado.
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    # Retorna o primeiro usuário da lista filtrada, se houver algum, ou retorna None se não houver nenhum.
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+# Função para criar uma nova conta e associá-la a um usuário.
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+    
+    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+
+# Função para listar todas as contas.
+def listar_contas(contas):
+    for conta in contas: 
+# Monta uma linha formatada com informações da conta, incluindo agência, número da conta e o nome do titular da conta, usando a sintaxe de f-string.
+        linha = f"""\
+            Agência:\t{conta['agencia']}
+            C/C:\t\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
+        print("=" * 100) # Imprime uma linha de igual (===) para separar as informações de cada conta.
+        print(textwrap.dedent(linha)) # Imprime as informações da conta, usando textwrap.dedent para formatar corretamente a string.
+
 # Função principal que gerencia as operações bancárias.
 def main():
     # Variáveis
@@ -66,8 +114,8 @@ def main():
     limite = 500  # Limite de saque
     extrato = ""  # Registro das transações
     numero_saques = 0  # Contador de saques
-    usuarios = [] 
-    contas = []
+    usuarios = [] # Armazena usuário
+    contas = [] # Armazena conta
 
     # Loop principal do programa
     while True:
@@ -95,6 +143,22 @@ def main():
         # Opção 3: Extrato
         elif opcao == "3":
             exibir_extrato(saldo, extrato=extrato)
+
+        # Opção 6: Novo usuário
+        elif opcao == "6":
+            criar_usuario(usuarios)
+
+        # Opção 4: Nova conta
+        elif opcao == "4":
+            numero_conta = len(contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
+
+        # Opção 5: Listar Contas
+        elif opcao == "5":
+            listar_contas(contas)
 
         # Opção 0: Sair
         elif opcao == "0":
